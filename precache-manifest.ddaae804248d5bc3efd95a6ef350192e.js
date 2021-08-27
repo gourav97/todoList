f5e70a38f406f0048c1e669b554d69cb6e1a5c89
Gourav <gouravapgis@gmail.com>
2021-08-27 23:03:40 +0530

    Updates

---
+++ precache-manifest.ddaae804248d5bc3efd95a6ef350192e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85fee5240cabb647ca7ab6e8e60af45c",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "6c61b145cef2a42e6936",
+    "url": "/todolist/static/css/2.fa6c921b.chunk.css"
+  },
+  {
+    "revision": "6c61b145cef2a42e6936",
+    "url": "/todolist/static/js/2.1bfa2620.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/todolist/static/js/2.1bfa2620.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f6d678066a0fbb0f9a6",
+    "url": "/todolist/static/js/main.86b5ca5a.chunk.js"
+  },
+  {
+    "revision": "7558fb00b4542e238607",
+    "url": "/todolist/static/js/runtime-main.97699757.js"
+  }
+]);
